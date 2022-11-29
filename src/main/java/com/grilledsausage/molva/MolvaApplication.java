0076f0e780beea7252f5fe0816c89e2052c15022
mingeun0507 <tqdlfkg@naver.com>
2022-11-29 13:04:18 +0900

    Fix: Main application 수정

---
+++ src/main/java/com/grilledsausage/molva/MolvaApplication.java
@@ -0,0 +1,13 @@
+package com.grilledsausage.molva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolvaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grilledsausage/molva/MolvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grilledsausage.molva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MolvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
