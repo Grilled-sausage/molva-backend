dd3b85d5ea079c63983204dc540aad176686aee7
mingeun0507 <tqdlfkg@naver.com>
2022-11-30 10:51:22 +0900

    Feat: ParticipationRepository 생성

---
+++ src/main/java/com/grilledsausage/molva/api/entity/participation/ParticipationRepository.java
@@ -0,0 +1,7 @@
+package com.grilledsausage.molva.api.entity.participation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParticipationRepository extends JpaRepository<Participation, Long> {
+
+}
