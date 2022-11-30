247f18daaba5f98253aaf4fa8ca7a5b517abe2b5
mingeun0507 <tqdlfkg@naver.com>
2022-11-30 10:17:52 +0900

    Feat: Rating 엔티티 추가

---
+++ src/main/java/com/grilledsausage/molva/api/entity/rating/Rating.java
@@ -0,0 +1,32 @@
+package com.grilledsausage.molva.api.entity.rating;
+
+import com.grilledsausage.molva.api.entity.movie.Movie;
+import com.grilledsausage.molva.api.entity.user.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "Reservation")
+public class Rating {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id", nullable = false)
+    private Movie movie;
+
+    @Column(name = "user_rating", nullable = false)
+    private Double userRating;
+
+}
