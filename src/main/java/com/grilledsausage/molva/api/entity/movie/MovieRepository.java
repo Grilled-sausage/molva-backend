6bf89f996ecf5e67e5bfa95410dc60d069256bd8
mingeun0507 <tqdlfkg@naver.com>
2022-11-30 10:50:32 +0900

    Feat: MovieRepository 생성

---
+++ src/main/java/com/grilledsausage/molva/api/entity/movie/MovieRepository.java
@@ -0,0 +1,7 @@
+package com.grilledsausage.molva.api.entity.movie;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+}
