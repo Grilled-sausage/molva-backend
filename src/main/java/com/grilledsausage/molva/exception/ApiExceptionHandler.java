46a59e6190d8841099f5b0eaccbc57e56a83b01a
mingeun0507 <tqdlfkg@naver.com>
2022-12-01 11:07:17 +0900

    Feat: ExceptionHandler에 로깅 기능 추가

--- src/main/java/com/grilledsausage/molva/exception/ApiExceptionHandler.java
+++ src/main/java/com/grilledsausage/molva/exception/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.grilledsausage.molva.exception;
 
 import com.grilledsausage.molva.exception.custom.CustomException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,11 +9,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @ControllerAdvice
 public class ApiExceptionHandler {
 
     @ExceptionHandler(value = {CustomException.class})
     public ResponseEntity<ApiException> handleUserNotExistException(CustomException e) {
+
+        log.error(e.getMessage());
 
         HttpStatus httpStatus = e.getHttpStatus();
 
