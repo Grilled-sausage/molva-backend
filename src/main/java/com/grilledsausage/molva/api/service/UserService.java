c689858477a6c6c8fd3c61259146d8ab4ab4bf26
mingeun0507 <tqdlfkg@naver.com>
2022-12-01 10:29:24 +0900

    Feat: JWT 토큰 생성하는 메소드 추가

--- src/main/java/com/grilledsausage/molva/api/service/UserService.java
+++ src/main/java/com/grilledsausage/molva/api/service/UserService.java
@@ -1,17 +1,26 @@
 package com.grilledsausage.molva.api.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.grilledsausage.molva.api.dto.OAuthToken;
+import com.grilledsausage.molva.api.dto.user.KakaoProfile;
+import com.grilledsausage.molva.api.dto.user.OAuthToken;
+import com.grilledsausage.molva.api.entity.user.User;
 import com.grilledsausage.molva.api.entity.user.UserRepository;
+import com.grilledsausage.molva.config.JwtProperties;
 import com.grilledsausage.molva.exception.custom.InvalidAuthorizationCodeException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Date;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -19,8 +28,10 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     @Value("${jwt.secret}")
-    private String SECRET;
+    private String JWT_SECRET;
 
     @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
     private String CLIENT_ID;
@@ -30,7 +41,6 @@ public class UserService {
 
     @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
     private String CLIENT_SECRET;
-
 
     public OAuthToken getAccessToken(String code) {
 
@@ -76,4 +86,66 @@ public class UserService {
 
         return oAuthToken;
     }
+
+    public String saveUserAndGetToken(String token) {
+
+        KakaoProfile profile = findProfile(token);
+
+        Optional<User> user = userRepository.findByEmail(profile.getKakao_account().getEmail());
+
+        if (user.isEmpty()) {
+            User newUser = User.oAuth2Register()
+                    .email(profile.getKakao_account().getEmail())
+                    .password(passwordEncoder.encode(profile.getId().toString()))
+                    .build();
+
+            userRepository.save(newUser);
+
+            return createToken(newUser);
+        }
+
+        return createToken(user.get());
+
+    }
+
+    public KakaoProfile findProfile(String token) {
+
+        RestTemplate rt = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + token); //(1-4)
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest =
+                new HttpEntity<>(headers);
+
+        ResponseEntity<String> kakaoProfileResponse = rt.exchange(
+                "https://kapi.kakao.com/v2/user/me",
+                HttpMethod.POST,
+                kakaoProfileRequest,
+                String.class
+        );
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        KakaoProfile kakaoProfile = null;
+        try {
+            kakaoProfile = objectMapper.readValue(kakaoProfileResponse.getBody(), KakaoProfile.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return kakaoProfile;
+
+    }
+
+    public String createToken(User user) {
+
+        return JWT.create()
+                .withSubject(user.getEmail())
+                .withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
+                .withClaim("email", user.getEmail())
+                .sign(Algorithm.HMAC512(JWT_SECRET));
+
+    }
+
 }
