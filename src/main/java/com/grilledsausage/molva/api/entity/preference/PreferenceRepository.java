ae97745d8e463fc6ea3c40536e5a48f6bb3fbd05
mingeun0507 <tqdlfkg@naver.com>
2022-12-21 01:16:00 +0900

    Feat: Preference Repository 추가

---
+++ src/main/java/com/grilledsausage/molva/api/entity/preference/PreferenceRepository.java
@@ -0,0 +1,7 @@
+package com.grilledsausage.molva.api.entity.preference;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PreferenceRepository extends JpaRepository<Preference, Long> {
+
+}
