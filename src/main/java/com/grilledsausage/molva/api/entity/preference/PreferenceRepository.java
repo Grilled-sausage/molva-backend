c3c4368ae9c38d704a6aa189f1353f3f61bcd60e
mingeun0507 <tqdlfkg@naver.com>
2022-12-21 16:32:13 +0900

    Feat: 선호 정보를 입력하는 메소드 구현

--- src/main/java/com/grilledsausage/molva/api/entity/preference/PreferenceRepository.java
+++ src/main/java/com/grilledsausage/molva/api/entity/preference/PreferenceRepository.java
@@ -2,6 +2,10 @@ package com.grilledsausage.molva.api.entity.preference;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PreferenceRepository extends JpaRepository<Preference, Long> {
+
+    Optional<Preference> findByUser_IdAndFilmmaker_Id(Long userId, Long filmmaker_id);
 
 }
