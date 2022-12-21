53616fe45daeb90f29e85b938d7591bf3ac5bf35
mingeun0507 <tqdlfkg@naver.com>
2022-12-21 11:24:53 +0900

    Feat: userId와 movieId로 Rating을 찾는 메소드 추가

--- src/main/java/com/grilledsausage/molva/api/entity/rating/RatingRepository.java
+++ src/main/java/com/grilledsausage/molva/api/entity/rating/RatingRepository.java
@@ -2,6 +2,10 @@ package com.grilledsausage.molva.api.entity.rating;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RatingRepository extends JpaRepository<Rating, Long> {
+
+    Optional<Rating> findByUser_IdAndMovie_Id(Long userId, Long movieId);
 
 }
