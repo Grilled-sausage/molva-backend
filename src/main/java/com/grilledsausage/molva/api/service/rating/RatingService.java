7038ce5f8a036c25b74e29513ec329f940d96bf2
mingeun0507 <tqdlfkg@naver.com>
2022-12-21 16:22:12 +0900

    Feat: 영화 평가 기능에서 사용하는 메소드 구현

--- src/main/java/com/grilledsausage/molva/api/service/rating/RatingService.java
+++ src/main/java/com/grilledsausage/molva/api/service/rating/RatingService.java
@@ -7,10 +7,14 @@ import com.grilledsausage.molva.api.entity.rating.Rating;
 import com.grilledsausage.molva.api.entity.rating.RatingRepository;
 import com.grilledsausage.molva.api.entity.user.User;
 import com.grilledsausage.molva.exception.custom.MovieNotFoundByIdException;
+import com.grilledsausage.molva.exception.custom.RatingNotFoundByIdException;
+import com.grilledsausage.molva.exception.custom.ValueOutOfBoundaryException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -20,8 +24,46 @@ public class RatingService {
 
     private final MovieRepository movieRepository;
 
-    @Transactional
     public Rating rateMovie(User user, MovieRatingRequestDto movieRatingRequestDto) {
+
+        Long existingRatingId = getExistingRatingId(user, movieRatingRequestDto);
+
+        validateValue(movieRatingRequestDto);
+
+        if (existingRatingId == -1) {
+            return makeNewRating(user, movieRatingRequestDto);
+        } else {
+            return updateExistingRating(user, movieRatingRequestDto, existingRatingId);
+        }
+
+    }
+
+    public void validateValue(MovieRatingRequestDto movieRatingRequestDto) {
+
+        if (movieRatingRequestDto.getMovieRating() > 5.0 || movieRatingRequestDto.getMovieRating() < 0.0) {
+            throw ValueOutOfBoundaryException
+                    .builder()
+                    .httpStatus(HttpStatus.BAD_REQUEST)
+                    .message("유효하지 않은 값입니다. 평점에 0에서 5 사이의 실수를 입력해주세요.")
+                    .build();
+        }
+
+    }
+
+    public Long getExistingRatingId(User user, MovieRatingRequestDto movieRatingRequestDto) {
+
+        Optional<Rating> ratingFromDto = ratingRepository.findByUser_IdAndMovie_Id(user.getId(), movieRatingRequestDto.getMovieId());
+
+        if (ratingFromDto.isEmpty()) {
+            return -1L;
+        }
+
+        return ratingFromDto.get().getId();
+
+    }
+
+    @Transactional
+    public Rating makeNewRating(User user, MovieRatingRequestDto movieRatingRequestDto) {
 
         Movie movieFromDto = movieRepository
                 .findById(movieRatingRequestDto.getMovieId())
@@ -39,6 +81,23 @@ public class RatingService {
                 .userRating(movieRatingRequestDto.getMovieRating())
                 .build()
         );
+
+    }
+
+    @Transactional
+    public Rating updateExistingRating(User user, MovieRatingRequestDto movieRatingRequestDto, Long existingRatingId) {
+
+        Rating existingRating = ratingRepository.findById(existingRatingId).orElseThrow(
+                () -> RatingNotFoundByIdException
+                        .builder()
+                        .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .message("Rating의 id로 해당하는 Rating을 찾을 수 없습니다.")
+                        .build()
+        );
+
+        existingRating.setUserRating(movieRatingRequestDto.getMovieRating());
+
+        return ratingRepository.save(existingRating);
 
     }
 
