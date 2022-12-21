89e6c2606cf3475ba80aa1196edd2abfbdbcd566
mingeun0507 <tqdlfkg@naver.com>
2022-12-21 16:02:52 +0900

    Feat: Table명 수정

--- src/main/java/com/grilledsausage/molva/api/entity/rating/Rating.java
+++ src/main/java/com/grilledsausage/molva/api/entity/rating/Rating.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name = "Reservation")
+@Table(name = "Rating")
 public class Rating {
 
     @Id
