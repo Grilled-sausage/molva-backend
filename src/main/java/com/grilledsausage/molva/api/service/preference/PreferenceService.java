61a7d2b53c266312af4a3f6ab8ce5c4ca36a9b06
mingeun0507 <tqdlfkg@naver.com>
2022-12-22 10:21:40 +0900

    Feat: 영화인 선호 여부 등록 및 삭제 기능을 위한 메소드 구현

--- src/main/java/com/grilledsausage/molva/api/service/preference/PreferenceService.java
+++ src/main/java/com/grilledsausage/molva/api/service/preference/PreferenceService.java
@@ -6,7 +6,9 @@ import com.grilledsausage.molva.api.entity.filmmaker.FilmmakerRepository;
 import com.grilledsausage.molva.api.entity.preference.Preference;
 import com.grilledsausage.molva.api.entity.preference.PreferenceRepository;
 import com.grilledsausage.molva.api.entity.user.User;
+import com.grilledsausage.molva.exception.custom.DuplicatedPreferenceException;
 import com.grilledsausage.molva.exception.custom.FilmmakerNotFoundByIdException;
+import com.grilledsausage.molva.exception.custom.PreferenceNotFoundByIdException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -23,6 +25,27 @@ public class PreferenceService {
     @Transactional
     public Preference preferFilmmaker(User user, Long filmmakerId) {
 
+        if (!isDuplicated(user, filmmakerId)) {
+            return makeNewPreference(user, filmmakerId);
+        } else {
+            throw DuplicatedPreferenceException
+                    .builder()
+                    .httpStatus(HttpStatus.BAD_REQUEST)
+                    .message("이미 동일한 사용자가 동일한 영화를 선호하고 있습니다.")
+                    .build();
+        }
+
+    }
+
+    private boolean isDuplicated(User user, Long filmmakerId) {
+
+        return preferenceRepository.existsByUser_IdAndFilmmaker_Id(user.getId(), filmmakerId);
+
+    }
+
+    @Transactional
+    protected Preference makeNewPreference(User user, Long filmmakerId) {
+
         Filmmaker filmmakerFromId = filmmakerRepository
                 .findById(filmmakerId)
                 .orElseThrow(
@@ -38,6 +61,23 @@ public class PreferenceService {
                 .filmmaker(filmmakerFromId)
                 .build()
         );
+
+    }
+
+    @Transactional
+    public void cancelPreferringFilmmaker(User user, Long filmmakerId) {
+
+        Preference existingPreference = preferenceRepository
+                .findByUser_IdAndFilmmaker_Id(user.getId(), filmmakerId)
+                .orElseThrow(
+                        () -> PreferenceNotFoundByIdException
+                                .builder()
+                                .httpStatus(HttpStatus.BAD_REQUEST)
+                                .message("filmmakerId에 해당하는 영화인 선호 정보를 찾을 수 없습니다.")
+                                .build()
+                );
+
+        preferenceRepository.deleteById(existingPreference.getId());
 
     }
 
