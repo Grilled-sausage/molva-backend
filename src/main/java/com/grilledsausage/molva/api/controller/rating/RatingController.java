89ed0b972c78bb347fe5778ba823f33691ff4847
mingeun0507 <tqdlfkg@naver.com>
2022-12-22 10:39:03 +0900

    Feat: 영화 선호 정보 삭제 기능 API 구현

--- src/main/java/com/grilledsausage/molva/api/controller/rating/RatingController.java
+++ src/main/java/com/grilledsausage/molva/api/controller/rating/RatingController.java
@@ -7,10 +7,7 @@ import com.grilledsausage.molva.api.service.rating.RatingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,6 +20,15 @@ public class RatingController {
     public ResponseEntity<MovieRatingResponseDto> rateMovie(@AuthenticationPrincipal User user, @RequestBody MovieRatingRequestDto movieRatingRequestDto) {
 
         return ResponseEntity.ok(MovieRatingResponseDto.from(ratingService.rateMovie(user, movieRatingRequestDto)));
+
+    }
+
+    @DeleteMapping("/movie")
+    public ResponseEntity<String> cancelRatingMovie(@AuthenticationPrincipal User user, @RequestBody Long movieId) {
+
+        ratingService.cancelRatingMovie(user, movieId);
+
+        return ResponseEntity.ok("success");
 
     }
 
