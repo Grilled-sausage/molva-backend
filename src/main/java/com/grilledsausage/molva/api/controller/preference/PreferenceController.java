e83424f1c630f213ab4b77014241df5055b6ffbe
mingeun0507 <tqdlfkg@naver.com>
2022-12-22 10:21:54 +0900

    Feat: 영화인 삭제 API 구현

--- src/main/java/com/grilledsausage/molva/api/controller/preference/PreferenceController.java
+++ src/main/java/com/grilledsausage/molva/api/controller/preference/PreferenceController.java
@@ -6,10 +6,7 @@ import com.grilledsausage.molva.api.service.preference.PreferenceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,6 +19,15 @@ public class PreferenceController {
     public ResponseEntity<FilmmakerPreferenceResponseDto> preferFilmmaker(@AuthenticationPrincipal User user, @RequestBody Long filmmakerId) {
 
         return ResponseEntity.ok(FilmmakerPreferenceResponseDto.from(preferenceService.preferFilmmaker(user, filmmakerId)));
+
+    }
+
+    @DeleteMapping("/filmmaker")
+    public ResponseEntity<String> deleteFilmmaker(@AuthenticationPrincipal User user, @RequestBody Long filmmakerId) {
+
+        preferenceService.cancelPreferringFilmmaker(user, filmmakerId);
+
+        return ResponseEntity.ok("success");
 
     }
 
