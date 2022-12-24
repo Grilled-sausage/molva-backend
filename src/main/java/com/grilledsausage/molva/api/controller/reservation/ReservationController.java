f9d51635018766ebb9ba2ca2c7269a4cb95b0b71
mingeun0507 <tqdlfkg@naver.com>
2022-12-24 17:08:41 +0900

    Feat: 영화 보고싶어요 표시 및 취소를 위한 API 구현

---
+++ src/main/java/com/grilledsausage/molva/api/controller/reservation/ReservationController.java
@@ -0,0 +1,32 @@
+package com.grilledsausage.molva.api.controller.reservation;
+
+import com.grilledsausage.molva.api.dto.reservation.MovieReservationResponseDto;
+import com.grilledsausage.molva.api.entity.user.User;
+import com.grilledsausage.molva.api.service.reservation.ReservationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/reservation")
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    @PostMapping("/movie")
+    public ResponseEntity<MovieReservationResponseDto> reserveMovie(@AuthenticationPrincipal User user, @RequestBody Long movieId) {
+
+        return ResponseEntity.ok(MovieReservationResponseDto.from(reservationService.reserveMovie(user, movieId)));
+    }
+
+    @DeleteMapping("/movie")
+    public ResponseEntity<String> cancelReservingMovie(@AuthenticationPrincipal User user, @RequestBody Long movieId) {
+        reservationService.cancelReservingMovie(user, movieId);
+
+        return ResponseEntity.ok("success");
+    }
+
+}
