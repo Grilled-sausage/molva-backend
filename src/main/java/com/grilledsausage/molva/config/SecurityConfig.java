5b10e7cd8d462bfc62fc68fd4391aa2511fd17c7
mingeun0507 <tqdlfkg@naver.com>
2022-12-24 19:38:45 +0900

    Fix: CORS disable 방식 변경

--- src/main/java/com/grilledsausage/molva/config/SecurityConfig.java
+++ src/main/java/com/grilledsausage/molva/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -43,8 +44,7 @@ public class SecurityConfig {
 
         http
                 .csrf().disable()
-                .cors().configurationSource(corsConfigurationSource())
-                .and()
+                .cors(AbstractHttpConfigurer::disable)
                 .formLogin().disable()
                 .authorizeRequests()
                 .antMatchers("/api/auth/token").permitAll()
