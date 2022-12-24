eb7a771263c91385894d347d2bec22fb9ef3d0b9
mingeun0507 <tqdlfkg@naver.com>
2022-12-24 15:24:15 +0900

    Fix: ContentController에 @CrossOrigin 어노테이션 추가

--- src/main/java/com/grilledsausage/molva/api/controller/content/ContentController.java
+++ src/main/java/com/grilledsausage/molva/api/controller/content/ContentController.java
@@ -4,6 +4,7 @@ import com.grilledsausage.molva.api.dto.content.SurveyFilmmakerResponseDto;
 import com.grilledsausage.molva.api.dto.content.SurveyMovieResponseDto;
 import com.grilledsausage.molva.api.service.content.ContentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
 @RequestMapping("/api/content")
 public class ContentController {
 
