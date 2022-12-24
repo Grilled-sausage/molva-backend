a3bdd264ea207b50106fc82ec78d39a67058ada3
mingeun0507 <tqdlfkg@naver.com>
2022-12-24 15:18:24 +0900

    Fix: UserController에 @CrossOrigin 어노테이션 추가

--- src/main/java/com/grilledsausage/molva/api/controller/user/UserController.java
+++ src/main/java/com/grilledsausage/molva/api/controller/user/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
 @RequestMapping("/api")
 public class UserController {
 
