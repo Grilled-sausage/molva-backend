2363df689b541bc0c7419f71b110abee0e510b1d
mingeun0507 <tqdlfkg@naver.com>
2022-12-25 19:13:18 +0900

    Feat: 선호하는 영화인을 불러올때 필요한 메소드 구현

--- src/main/java/com/grilledsausage/molva/api/entity/preference/PreferenceRepository.java
+++ src/main/java/com/grilledsausage/molva/api/entity/preference/PreferenceRepository.java
@@ -2,6 +2,7 @@ package com.grilledsausage.molva.api.entity.preference;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PreferenceRepository extends JpaRepository<Preference, Long> {
@@ -9,5 +10,7 @@ public interface PreferenceRepository extends JpaRepository<Preference, Long> {
     Optional<Preference> findByUser_IdAndFilmmaker_Id(Long userId, Long filmmaker_id);
 
     Boolean existsByUser_IdAndFilmmaker_Id(Long userId, Long filmmaker_id);
+
+    List<Preference> findAllByUser_Id(Long userId);
 
 }
