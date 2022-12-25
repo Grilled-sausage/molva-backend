c2cdc495b65d5ed2f2c0f1d87de01e8d1ad08f4c
mingeun0507 <tqdlfkg@naver.com>
2022-12-25 19:13:30 +0900

    Feat: 선호하는 영화인을 불러올때 필요한 API 구현

--- src/main/java/com/grilledsausage/molva/api/controller/preference/PreferenceController.java
+++ src/main/java/com/grilledsausage/molva/api/controller/preference/PreferenceController.java
@@ -1,6 +1,7 @@
 package com.grilledsausage.molva.api.controller.preference;
 
 import com.grilledsausage.molva.api.dto.preference.FilmmakerPreferenceResponseDto;
+import com.grilledsausage.molva.api.dto.preference.GetPreferredFilmmakersResponseDto;
 import com.grilledsausage.molva.api.entity.user.User;
 import com.grilledsausage.molva.api.service.preference.PreferenceService;
 import lombok.RequiredArgsConstructor;
@@ -8,12 +9,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/preference")
 public class PreferenceController {
 
     private final PreferenceService preferenceService;
+
+    @GetMapping("/filmmaker")
+    public ResponseEntity<List<GetPreferredFilmmakersResponseDto>> getPreferredFilmmaker(@AuthenticationPrincipal User user, @RequestParam(name = "type") String type) {
+        return ResponseEntity.ok(preferenceService.getPreferredFilmmaker(user, type));
+    }
 
     @PostMapping("/filmmaker")
     public ResponseEntity<FilmmakerPreferenceResponseDto> preferFilmmaker(@AuthenticationPrincipal User user, @RequestBody Long filmmakerId) {
