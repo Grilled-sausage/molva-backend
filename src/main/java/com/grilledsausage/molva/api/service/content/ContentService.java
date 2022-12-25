91fa7866270526d859a2eaecb3455aeed40b3d8e
mingeun0507 <tqdlfkg@naver.com>
2022-12-25 22:16:07 +0900

    Fix: Movie 상세 정보를 받아오는 Service 로직 수정

--- src/main/java/com/grilledsausage/molva/api/service/content/ContentService.java
+++ src/main/java/com/grilledsausage/molva/api/service/content/ContentService.java
@@ -1,11 +1,19 @@
 package com.grilledsausage.molva.api.service.content;
 
+import com.grilledsausage.molva.api.dto.content.MovieInfoResponseDto;
 import com.grilledsausage.molva.api.dto.content.SurveyFilmmakerResponseDto;
 import com.grilledsausage.molva.api.dto.content.SurveyMovieResponseDto;
 import com.grilledsausage.molva.api.entity.filmmaker.Filmmaker;
 import com.grilledsausage.molva.api.entity.filmmaker.FilmmakerRepository;
 import com.grilledsausage.molva.api.entity.movie.Movie;
 import com.grilledsausage.molva.api.entity.movie.MovieRepository;
+import com.grilledsausage.molva.api.entity.participation.Participation;
+import com.grilledsausage.molva.api.entity.participation.ParticipationRepository;
+import com.grilledsausage.molva.api.entity.preference.PreferenceRepository;
+import com.grilledsausage.molva.api.entity.rating.Rating;
+import com.grilledsausage.molva.api.entity.rating.RatingRepository;
+import com.grilledsausage.molva.api.entity.reservation.ReservationRepository;
+import com.grilledsausage.molva.api.entity.user.User;
 import com.grilledsausage.molva.exception.custom.MovieNotFoundByIdException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,6 +29,14 @@ public class ContentService {
     private final MovieRepository movieRepository;
 
     private final FilmmakerRepository filmmakerRepository;
+
+    private final RatingRepository ratingRepository;
+
+    private final ReservationRepository reservationRepository;
+
+    private final PreferenceRepository preferenceRepository;
+
+    private final ParticipationRepository participationRepository;
 
     public List<SurveyMovieResponseDto> getSurveyMovieList() {
 
@@ -39,15 +55,67 @@ public class ContentService {
     }
 
 
-    public Movie getMovieInfo(Long movieId) {
+    public MovieInfoResponseDto getMovieInfo(User user, Long movieId) {
 
-        return movieRepository.findById(movieId).orElseThrow(
+        Movie movieFromUserAndId = movieRepository.findById(movieId).orElseThrow(
                 () -> MovieNotFoundByIdException
                         .builder()
                         .httpStatus(HttpStatus.BAD_REQUEST)
                         .message("주어진 movieId에 해당하는 영화가 없습니다.")
                         .build()
         );
+
+        // 1. movieId로 부터 movie를 받아와서 DTO로 가공.
+        MovieInfoResponseDto movieInfoResponseDtoFromId = MovieInfoResponseDto.from(movieFromUserAndId);
+
+        // 2. movieDto에 myRating 넣기
+        movieInfoResponseDtoFromId.setMyRating(
+                ratingRepository.findByUser_IdAndMovie_Id(
+                        user.getId(), movieId).orElse(
+                        Rating.builder().userRating(0.0).build()
+                ).getUserRating()
+        );
+
+        // 3. movieDto에 reviewRating (평균) 넣기
+        movieInfoResponseDtoFromId.setReviewRating(
+                movieFromUserAndId
+                        .getRatings().stream().map(Rating::getUserRating)
+                        .mapToDouble(x -> x).summaryStatistics().getAverage()
+        );
+
+        // 4. movieDto에 감독 넣기
+        movieInfoResponseDtoFromId.setDirectorInfoDto(
+                MovieInfoResponseDto.FilmmakerInfoDto.from(movieFromUserAndId
+                        .getParticipations().stream().map(Participation::getFilmmaker)
+                        .filter(x -> x.getType().equals("감독")).findFirst().orElse(
+                                Filmmaker
+                                        .builder()
+                                        .id(999L)
+                                        .name("없음")
+                                        .type("감독")
+                                        .image("없음")
+                                        .code(999L)
+                                        .build()
+                        )
+                )
+        );
+
+        // 5. movieDto에 배우들 넣기
+        List<MovieInfoResponseDto.FilmmakerInfoDto> actorInfoDtoList =
+                movieFromUserAndId
+                        .getParticipations().stream().map(Participation::getFilmmaker)
+                        .filter(x -> x.getType().equals("배우")).map(MovieInfoResponseDto.FilmmakerInfoDto::from)
+                        .collect(Collectors.toList());
+
+        movieInfoResponseDtoFromId
+                .setActorInfoDtoList(actorInfoDtoList.subList(0, 6));
+
+        // 6. FilmmakerDto에 선호 여부 넣기
+        movieInfoResponseDtoFromId.getDirectorInfoDto().setIsPreferred(
+                user.getPreferences().stream().anyMatch(x -> x.getFilmmaker().getId().equals(movieInfoResponseDtoFromId.getDirectorInfoDto().getId()))
+        );
+
+        return movieInfoResponseDtoFromId;
 
     }
 }
