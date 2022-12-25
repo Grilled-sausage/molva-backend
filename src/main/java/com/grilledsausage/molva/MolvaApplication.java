95677a6b9042266faf89d3475f3cee34f15643af
mingeun0507 <tqdlfkg@naver.com>
2022-12-25 12:11:28 +0900

    Feat: 전역적 WebMvcConfigurer 추가

--- src/main/java/com/grilledsausage/molva/MolvaApplication.java
+++ src/main/java/com/grilledsausage/molva/MolvaApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MolvaApplication {
@@ -12,6 +14,21 @@ public class MolvaApplication {
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("*")
+						.allowCredentials(true)
+						.maxAge(3000);
+			}
+		};
 	}
 
 	public static void main(String[] args) {
