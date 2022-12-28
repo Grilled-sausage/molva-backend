b59a3abfc89429d0cbec9c90200f45bdd01c6ddf
mingeun0507 <tqdlfkg@naver.com>
2022-12-28 22:41:11 +0900

    Feat: Code로 찾는 메소드 생성

--- src/main/java/com/grilledsausage/molva/api/entity/movie/MovieRepository.java
+++ src/main/java/com/grilledsausage/molva/api/entity/movie/MovieRepository.java
@@ -3,11 +3,14 @@ package com.grilledsausage.molva.api.entity.movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     List<Movie> findAllByIsInSurveyTrue();
 
     List<Movie> findAllByNameContains(String keyword);
+
+    Optional<Movie> findByCode(Long code);
 
 }
